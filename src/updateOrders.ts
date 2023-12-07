451b801d9eef9fe5ec3d216ee1cfe64e12858074
Dom-Mac <domenicochad@gmail.com>
2023-12-07 01:38:59 +0100

    url

--- src/updateOrders.ts
+++ src/updateOrders.ts
@@ -1,6 +1,8 @@
 async function updateOrders() {
-  // const res = await fetch("https://slice.so/api/orders/basedMerch")
-  const res = await fetch("https://base-testnet.slice.so/api/orders/basedMerch")
+  // const res = await fetch("https://slice.so/api/orders/storeBasedMerch")
+  const res = await fetch(
+    "https://base-testnet.slice.so/api/orders/storeBasedMerch"
+  )
 
   if (!res.ok) {
     console.log("Could not fetch products")
